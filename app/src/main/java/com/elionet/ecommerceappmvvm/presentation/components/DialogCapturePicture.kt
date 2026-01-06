f48b504168c5769089aadfc8e1fe74449d4b67cc
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2026-01-05 22:57:30 -0500

    ok ok ok

--- app/src/main/java/com/elionet/ecommerceappmvvm/presentation/components/DialogCapturePicture.kt
+++ app/src/main/java/com/elionet/ecommerceappmvvm/presentation/components/DialogCapturePicture.kt
@@ -3,7 +3,10 @@ package com.elionet.ecommerceappmvvm.presentation.components
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -32,8 +35,34 @@ fun DialogCapturePicture(
                     color = Color.Black
                 )
             },
-            buttons = {
-                Row(modifier = Modifier.fillMaxWidth()) {)
-
+            text = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 30.dp)
+                ) {
+                    Button(
+                        modifier = Modifier
+                            .width(130.dp),
+                        onClick = {
+                            state.value = false
+                            pickImage()
+                        }
+                    ){
+                        Text(text = "Galeria")
+                    }
+                    Button(
+                        modifier = Modifier
+                            .width(130.dp),
+                        onClick = {
+                            state.value = false
+                            takePhoto()
+                        }
+                    ){
+                        Text(text = "Camara")
+                    }
+                }
+         }
+        )
     }
 }
